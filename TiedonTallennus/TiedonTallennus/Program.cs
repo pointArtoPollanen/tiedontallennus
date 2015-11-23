a7066a2f33d958dae54c255ce22b8fb3187eaab5
Arto Pöllänen <arto.pollanen@stud.pointcollege.fi>
2015-11-23 15:25:04 +0200

    Syöttö lisätty

--- TiedonTallennus/TiedonTallennus/Program.cs
+++ TiedonTallennus/TiedonTallennus/Program.cs
@@ -11,12 +11,20 @@ namespace TiedonTallennus
    {
       static void Main(string[] args)
       {
+         string tiedosto = "C:\\temp\\Lämpötila.txt";
+
+         // luetaan tiedostosta aiemmin asetettu lämpötila
+         if (File.Exists(tiedosto))
+         {
+            string aiempiArvo = File.ReadAllText(tiedosto);
+            Console.WriteLine("Aiemmin asetettu lämpötila on " + aiempiArvo + " astetta.");
+         }
+
          int lämpötila = 0;
          Console.Write("Anna usi lämpötila: ");
          string syöte = Console.ReadLine();
          lämpötila = int.Parse(syöte);
 
-         string tiedosto = "C:\\temp\\Lämpötila.txt";
          File.WriteAllText(tiedosto, lämpötila.ToString());
          Console.WriteLine("Uusi lämpötila tallennettu.");
 
