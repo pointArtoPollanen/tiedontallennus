4433c9dc97e836dcdb5b6809718b8457ef79c92c
Arto Pöllänen <arto.pollanen@stud.pointcollege.fi>
2015-11-23 13:48:57 +0200

    Ensimmäinen versio

---
+++ TiedonTallennus/TiedonTallennus/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiedonTallennus
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+      }
+   }
+}
